d5088767e39855cff9c70ee2145c81653930a8a5
kimhyeok-cloud <rlafur515@gmail.com>
2025-10-11 17:57:39 +0000

    my first code

---
+++ chap4/prob6/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+
+#include <stdlib.h>
+#include "student.h"
+int main(int argc, char* argv[])  
+{ 
+   struct student rec;
+   FILE *fp;
+
+   if (argc != 2) {
+      fprintf(stderr, "How to use: %s FileName\n",argv[0]);
+      exit(1); 
+   }
+
+   fp = fopen(argv[1], "w");
+   printf("%-9s %-7s %-4s\n", "StudentID", "Name", "Score"); 
+   while (scanf("%d %s %hd", &rec.id, rec.name, &rec.score) == 3){ 
+      fprintf(fp, "%d %s %hd\n", rec.id, rec.name, rec.score);
+   }
+
+   fclose(fp);
+   exit(0);
+} 
+
